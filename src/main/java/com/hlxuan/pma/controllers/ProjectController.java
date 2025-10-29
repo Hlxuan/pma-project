e93d6197d08f3b2528e365029be3df8fcd9cf108
Hlxuan <huanglangxuan@hlxuan.top>
2025-10-29 11:13:59 +0800

    update: 绑定Java对象到Thymeleaf表单 & 创建Project实体的CrudRepository

---
+++ src/main/java/com/hlxuan/pma/controllers/ProjectController.java
@@ -0,0 +1,33 @@
+package com.hlxuan.pma.controllers;
+
+import com.hlxuan.pma.dao.ProjectRepository;
+import com.hlxuan.pma.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("projects")
+public class ProjectController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @GetMapping("/new")
+    public String displayProjectForm(Model model){
+
+        Project aProject = new Project();
+        model.addAttribute("project", aProject);
+        return "new-project";
+    }
+
+    @PostMapping("/save")
+    public String createProject(Model model, Project project){
+
+        projectRepository.save(project);
+        return "redirect:/projects/new";
+    }
+}
---
+++ src/main/java/com/hlxuan/pma/dao/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.hlxuan.pma.dao;
+
+import com.hlxuan.pma.entities.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+}
