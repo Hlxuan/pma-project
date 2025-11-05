8be99c6ba6510a0c83014c62b9138c231598e0bb
Hlxuan <huanglangxuan@hlxuan.top>
2025-11-05 10:46:13 +0800

    update: 学生列表页面&项目列表页面&动态链接

--- src/main/java/com/hlxuan/pma/controllers/ProjectController.java
+++ src/main/java/com/hlxuan/pma/controllers/ProjectController.java
@@ -2,6 +2,7 @@ package com.hlxuan.pma.controllers;
 
 import com.hlxuan.pma.dao.ProjectRepository;
 import com.hlxuan.pma.entities.Project;
+import com.hlxuan.pma.entities.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("projects")
 public class ProjectController {
 
     @Autowired
     ProjectRepository projectRepository;
+
+    @GetMapping
+    public String displayProjects(Model model){
+
+        List<Project> projects = projectRepository.findAll();
+        model.addAttribute("projects", projects);
+        return "projects/list-projects";
+    }
 
     @GetMapping("/new")
     public String displayProjectForm(Model model){
