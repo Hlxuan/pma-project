9f395d8b6fe85c4995988f05e20061c7f88bc381
Hlxuan <huanglangxuan@hlxuan.top>
2025-11-14 23:27:12 +0800

    update: 使用Model传递数据到Thymeleaf模板，并在JS脚本中使用

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,13 +1,22 @@
+let charDataStr = decodeHtml(chartData)
+let chartJsonArray = JSON.parse(charDataStr)
+
+let arrayLength = chartJsonArray.length
+let numbericData = []
+let labelData = []
+
+for(let i = 0; i < arrayLength; i++){
+    numbericData[i] = chartJsonArray[i].value
+    labelData[i] = chartJsonArray[i].label
+}
+
+
 // 饼图
 const data = {
-    labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-    ],
+    labels: labelData,
     datasets: [{
         label: 'My First Dataset',
-        data: [300, 50, 100],
+        data: numbericData,
         backgroundColor: [
             'rgb(255, 99, 132)',
             'rgb(54, 162, 235)',
@@ -17,7 +26,25 @@ const data = {
     }]
 };
 
+// 配置选项
+const options = {
+    plugins:{
+        title:{
+            display: true,
+            text: '项目状态',
+        }
+    }
+}
+
 new Chart(document.getElementById("myPieChart"), {
     type: 'pie',
     data: data,
+    options: options
 })
+
+// [{"value": 1, "label":"未开始"}, {"value": 2, "label":"进行中"}, {"value": 3, "label":"已完成"}]
+function decodeHtml(html){
+    let txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
