ee3b1f3d6867273815a5605cf2fa71e7147d25f5
Hlxuan <huanglangxuan@hlxuan.top>
2025-11-22 14:35:26 +0800

    update: Spring AOP 2: 使用@Around, JoinPoint和ProceedingJoinPoint

--- src/main/java/com/hlxuan/pma/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/hlxuan/pma/logging/ApplicationLoggerAspect.java
@@ -1,12 +1,13 @@
 package com.hlxuan.pma.logging;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -19,15 +20,61 @@ public class ApplicationLoggerAspect {
 
     }
 
-    @Before("definePackagePointcuts()")
-    public void log1(){
+//    @Before("definePackagePointcuts()")
+//    public void logBefore(JoinPoint joinPoint){
+//        log.debug("\n\n\n");
+//        log.debug("********************方法执行前********************");
+//        log.debug(
+//                "{}.{}(), 输入参数{}",
+//                joinPoint.getSignature().getDeclaringTypeName(),
+//                joinPoint.getSignature().getName(),
+//                Arrays.toString(joinPoint.getArgs())
+//        );
+//        log.debug("--------------------------------------------------");
+//        log.debug("\n\n\n");
+//    }
+//
+//
+//    @After("definePackagePointcuts()")
+//    public void logAfter(JoinPoint joinPoint) {
+//        log.debug("\n\n\n");
+//        log.debug("********************方法执行后********************");
+//        log.debug(
+//                "{}.{}(), 输出参数{}",
+//                joinPoint.getSignature().getDeclaringTypeName(),
+//                joinPoint.getSignature().getName(),
+//                Arrays.toString(joinPoint.getArgs())
+//        );
+//        log.debug("--------------------------------------------------");
+//        log.debug("\n\n\n");
+//    }
+
+    @Around("definePackagePointcuts()")
+    public Object logAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        log.debug("\n\n\n");
+        log.debug("********************方法执行前********************");
+        log.debug(
+                "{}.{}(), 输入参数{}",
+                proceedingJoinPoint.getSignature().getDeclaringTypeName(),
+                proceedingJoinPoint.getSignature().getName(),
+                Arrays.toString(proceedingJoinPoint.getArgs())
+        );
         log.debug("--------------------------------------------------");
-    }
+        log.debug("\n\n\n");
 
+        Object object = proceedingJoinPoint.proceed();
 
-    @After("definePackagePointcuts()")
-    public void log(){
+        log.debug("\n\n\n");
+        log.debug("********************方法执行后********************");
+        log.debug(
+                "{}.{}(), 输出参数{}",
+                proceedingJoinPoint.getSignature().getDeclaringTypeName(),
+                proceedingJoinPoint.getSignature().getName(),
+                Arrays.toString(proceedingJoinPoint.getArgs())
+        );
         log.debug("--------------------------------------------------");
-    }
+        log.debug("\n\n\n");
 
+        return object;
+    }
 }
