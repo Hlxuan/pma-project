8839bf99da6138b6915b4aec2d94a980c654a3f6
Hlxuan <huanglangxuan@hlxuan.top>
2025-11-22 11:59:04 +0800

    update: 使用@SpringBootTest注解

--- src/test/java/com/hlxuan/pma/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/hlxuan/pma/ProjectRepositoryIntegrationTest.java
@@ -1,12 +1,12 @@
-package com.hlxuan.dao;
+package com.hlxuan.pma;
 
-import com.hlxuan.pma.PmaApplication;
 import com.hlxuan.pma.dao.ProjectRepository;
 import com.hlxuan.pma.entities.Project;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -14,9 +14,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@ContextConfiguration(classes = PmaApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({
         @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:drop.sql"),
         @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"})
